e307a137c1e0422e189dbb02c40397aac9c374ee
tinazhen08 <158211507+tinazhen08@users.noreply.github.com>
2024-10-31 13:58:35 -0400

    add buttons

--- app/JS/dom.js
+++ app/JS/dom.js
@@ -1,5 +1,10 @@
 const DOMSelector = {
     container: document.querySelector(".container"),
-    
+    movBtn: document.querySelector(".movies"),
+    shoBtn: document.querySelector(".shows"),
+    docBtn: document.querySelector(".documentaries"),
+    podBtn: document.querySelector(".podcasts"),
+    allBtn: document.querySelector(".all"),
 };
 
+export {DOMSelector};
--- app/JS/main.js
+++ app/JS/main.js
@@ -1,2 +1,4 @@
 import '../css/style.css';
+import { DOMSelector } from "./dom";
+import { movies, shows, documentaries, podcasts } from './media';
 
