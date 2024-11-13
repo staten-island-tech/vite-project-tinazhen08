c2ca28b4f8045c186441850207009fdd8720b1b1
tinazhen08 <158211507+tinazhen08@users.noreply.github.com>
2024-11-13 06:13:23 -0500

    finish themes

--- app/JS/main.js
+++ app/JS/main.js
@@ -11,7 +11,7 @@ function card(x) {
         <img src="${item.url}" alt="" class="card-img"></img>
         <h4 class="description">${item.description}</h4>
         <h4 class="release">${item.release}</h4>
-        <h3 class="price">${item.price}</h3>
+        <h3 class="price">$${item.price}</h3>
       </div>`
     )
   );
@@ -253,4 +253,17 @@ function media() {
   });
 }
 
+function theme() {
+  document.querySelector(".btn").addEventListener("click", function () {
+    if (document.body.classList.contains("light")) {
+      document.body.classList.add("dark");
+      document.body.classList.remove("light");
+    } else {
+      document.body.classList.add("light");
+      document.body.classList.remove("dark");
+    }
+  });
+}
+
 media();
+theme();
